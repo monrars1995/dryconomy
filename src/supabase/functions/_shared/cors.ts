34c01ca0f4eb223dd4669c2e33be3f743429bbcd
monrars1995 <monrars95@gmail.com>
2025-06-12 22:58:06 -0300

    Fix CORS configuration for localhost development

--- src/supabase/functions/_shared/cors.ts
+++ src/supabase/functions/_shared/cors.ts
@@ -1,9 +1,8 @@
 // src/functions/_shared/cors.ts
 export const corsHeaders = {
-    'Access-Control-Allow-Origin': 'https://dryconomy.mecalor.com',
+    'Access-Control-Allow-Origin': 'https://localhost:9001',
     'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
     'Access-Control-Allow-Credentials': 'true',
     'Content-Type': 'application/json'
   };
-  
